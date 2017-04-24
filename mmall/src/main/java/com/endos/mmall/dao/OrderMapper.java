51adeb58e2d163cca978d5596738b4a1c4c74b8a
endos <neverendos@163.com>
2017-04-24 15:58:35 +0900

    配置好Mybatis-generator并自动生成dao entity mapper文件

---
+++ mmall/src/main/java/com/endos/mmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.endos.mmall.dao;
+
+import com.endos.mmall.entity.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ mmall/src/main/java/com/endos/mmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.endos.mmall.dao;
+
+import com.endos.mmall.entity.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
